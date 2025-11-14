d0ea62c414578296b2046f34148cbd1a7ec53478
Zeynep Goktepe <129191416+Zeynoyder1@users.noreply.github.com>
2025-11-13 21:13:08 -0800

    Implement Caesar cipher decryption in assembly

---
+++ decrypt.s
@@ -0,0 +1,95 @@
+        .equ    SYS_READ,   63
+        .equ    SYS_WRITE,  64
+        .equ    SYS_EXIT,   93
+
+        .equ    STDIN,  0
+        .equ    STDOUT, 1
+
+        .equ    SHIFT,  3          // must match the encrypt shift
+
+        .section .bss
+        .align  3
+buf:    .skip   2048               // input/output buffer
+
+        .section .text
+        .global _start
+
+_start:
+        // read into buf
+        mov     x0, #STDIN
+        adrp    x1, buf
+        add     x1, x1, :lo12:buf
+        mov     x2, #2048
+        mov     x8, #SYS_READ
+        svc     #0
+
+        // x0 = bytes_read
+        mov     x19, x0            // save length
+        adrp    x20, buf
+        add     x20, x20, :lo12:buf
+
+        // process: Caesar shift on letters (decrypt)
+        mov     x21, #0            // i = 0
+        mov     w22, #SHIFT        // w22 = shift
+
+loop:
+        cmp     x21, x19
+        b.ge    done_process
+
+        // load byte b = buf[i]
+        ldrb    w0, [x20, x21]
+
+        // if 'A'<=b<='Z'
+        mov     w1, w0
+        cmp     w1, #'A'
+        b.lt    check_lower
+        cmp     w1, #'Z'
+        b.gt    check_lower
+        // uppercase mapping (reverse shift)
+        sub     w1, w1, #'A'               // 0..25
+        sub     w1, w1, w22                // -shift
+        cmp     w1, #0
+        b.ge    u_no_wrap
+        add     w1, w1, #26
+u_no_wrap:
+        add     w1, w1, #'A'
+        b       store
+
+check_lower:
+        // if 'a'<=b<='z'
+        mov     w1, w0
+        cmp     w1, #'a'
+        b.lt    store_orig
+        cmp     w1, #'z'
+        b.gt    store_orig
+        // lowercase mapping (reverse shift)
+        sub     w1, w1, #'a'
+        sub     w1, w1, w22
+        cmp     w1, #0
+        b.ge    l_no_wrap
+        add     w1, w1, #26
+l_no_wrap:
+        add     w1, w1, #'a'
+        b       store
+
+store_orig:
+        mov     w1, w0
+
+store:
+        strb    w1, [x20, x21]
+        add     x21, x21, #1
+        b       loop
+
+done_process:
+        // write buf back out
+        mov     x0, #STDOUT
+        adrp    x1, buf
+        add     x1, x1, :lo12:buf
+        mov     x2, x19
+        mov     x8, #SYS_WRITE
+        svc     #0
+
+        // exit(0)
+        mov     x0, #0
+        mov     x8, #SYS_EXIT
+        svc     #0
